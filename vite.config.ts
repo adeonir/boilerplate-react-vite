85f92aa1853a017a57faa53a41a3dd6d558b72e9
Adeonir Kohl <adeonir@gmail.com>
2022-04-17 11:19:22 -0300

    Install vite-plugin-linter

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsConfigPaths from 'vite-tsconfig-paths'
+import { EsLinter, linterPlugin, TypeScriptLinter } from 'vite-plugin-linter'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsConfigPaths()],
-})
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    linterPlugin({
+      include: ['./src/**/*.{ts,tsx}'],
+      linters: [new EsLinter({ configEnv }), new TypeScriptLinter()],
+    }),
+  ],
+}))
