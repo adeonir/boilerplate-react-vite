0707079b1ab7c824f4254b55581b03ef3f2ab3b2
Adeonir Kohl <adeonir@gmail.com>
2022-04-16 21:31:30 -0300

    Install eslint, prettier, husky and lint-staged

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsConfigPaths(),]
+  plugins: [react(), tsConfigPaths()],
 })
