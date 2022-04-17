cf1614a04aa78da221e18dff9b47126c56b01eb3
Adeonir Kohl <adeonir@gmail.com>
2022-04-17 17:59:46 -0300

    Fix lint

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
-import { GlobalStyle, theme } from '~/styles'
 
 import { App } from './App'
+
+import { GlobalStyle, theme } from '~/styles'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
