eb5253ef335a0eb7a879fb2aa3e6bae9478430c9
Adeonir Kohl <adeonir@gmail.com>
2022-04-26 12:15:33 -0300

    Add routes

---
+++ src/router/router.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
+
+import { Base } from '~/layouts/Base'
+
+import { About } from '~/pages/About'
+import { Home } from '~/pages/Home'
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route element={<Base />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Route>
+    </Routes>
+  )
+}
