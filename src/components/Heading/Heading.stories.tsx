75d473e4e404e5ceb259c94c6da5d125ce8dfe0e
Adeonir Kohl <adeonir@gmail.com>
2022-05-21 19:18:40 -0300

    Update stories

--- src/components/Heading/Heading.stories.tsx
+++ src/components/Heading/Heading.stories.tsx
@@ -5,11 +5,18 @@ import { Heading } from './Heading'
 export default {
   title: 'Heading',
   component: Heading,
+  argTypes: {
+    align: {
+      defaultValue: 'left',
+    },
+    as: {
+      defaultValue: 'h1',
+    },
+  },
 } as ComponentMeta<typeof Heading>
 
-const Template: ComponentStory<typeof Heading> = (args) => <Heading {...args} />
+const Template: ComponentStory<typeof Heading> = (args) => (
+  <Heading {...args}>Heading</Heading>
+)
 
 export const Primary = Template.bind({})
-Primary.args = {
-  children: 'Heading',
-}
