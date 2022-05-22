122bde89484cadaf2b7b03a98f54528cadd33fc3
Adeonir Kohl <adeonir@gmail.com>
2022-05-21 22:26:51 -0300

    Remove unused config

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,11 @@ import { resolve as pathResolve } from 'path'
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import { ViteAliases } from 'vite-aliases'
 
 const resolve = (path: string) => pathResolve(__dirname, path)
 
 export default defineConfig({
-  plugins: [
-    react(),
-    ViteAliases({
-      dir: 'src',
-      prefix: '~',
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '~': resolve('src'),
