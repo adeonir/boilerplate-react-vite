0ee0d6378e56a587443218071bc72824b890166e
Adeonir Kohl <adeonir@gmail.com>
2022-09-04 15:49:42 -0300

    Update packages

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
   },
   test: {
     globals: true,
+    watch: false,
     environment: 'jsdom',
     setupFiles: './vitest.setup.ts',
     resolveSnapshotPath: (testPath, snapExtension) => testPath + snapExtension,
