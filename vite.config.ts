21167043a47861e2324cfae44f4fc788dea3c0fd
Adeonir Kohl <adeonir@gmail.com>
2022-10-11 11:50:07 -0300

    Update settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,15 @@ import { resolve as pathResolve } from 'path'
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
 const resolve = (path: string) => pathResolve(__dirname, path)
 
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       '~': resolve('src'),
